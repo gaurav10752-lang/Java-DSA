5a9707c528dd97b3714e607e3e81f8836d85228e
gaurav10752-lang <gaurav10752@gmail.com>
2026-07-25 18:46:40 +0530

    Create 01_ArrayInitialization.java

---
+++ 01_ArrayInitialization.java
@@ -0,0 +1,8 @@
+public class ArrayInitialization{
+  public Static void main(String[]args){
+    int []arr={10,4,8,2,5};
+    for(int i=0; i<arr.length; i++){
+      System.out.print(arr[i]+"")
+        }
+  }
+}
