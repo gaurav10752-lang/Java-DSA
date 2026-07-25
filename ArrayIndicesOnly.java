a002896bccbd94c5e756cdb637b0d12b8bf95922
gaurav10752-lang <gaurav10752@gmail.com>
2026-07-25 18:55:50 +0530

    Create ArrayIndicesOnly.java

---
+++ ArrayIndicesOnly.java
@@ -0,0 +1,9 @@
+public class ArrayIndicesOnly{
+    public static void main(String[]args){
+        int arr[]={10,20,23,40};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(i);
+        }
+    }
+    
+}
