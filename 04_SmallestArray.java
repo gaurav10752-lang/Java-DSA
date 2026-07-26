61db769d1227df4bded77bbff75175cf7fab92b4
gaurav10752-lang <gaurav10752@gmail.com>
2026-07-26 19:07:27 +0530

    Create 04_SmallestArray.java

---
+++ 04_SmallestArray.java
@@ -0,0 +1,15 @@
+public class arrey {
+    public static void main(String[]args){
+        int arr[]={100,20,30,70,60,50};
+        int smallest=arr[0];
+        for(int i=0; i<6; i++ ){
+          if(arr[i]<smallest){
+            smallest=arr[i];
+          }
+
+        }
+        System.out.println(smallest);
+        }
+
+
+        }
